8b63d0ef67d19529fd75512ae4f939e117900361
miles-blaq <gerald_miles23@yahoo.com>
2022-10-25 08:33:09 +0100

    changes to config file/index

--- da-task-1/config/index.js
+++ da-task-1/config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "http://myVercelServer.com";
+// export const server = dev ? "http://localhost:3000" : "http://myVercelServer.com";
+export const server = "http://localhost:3000";
